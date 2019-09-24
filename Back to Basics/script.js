59f85502280dd8c165e9914a7bd0826b6c1fa751
racheldmiller <rachelmiller415@gmail.com>
2019-09-23 23:08:47 -0700

    getting my feet wet again with more documentaiton

---
+++ Back to Basics/script.js
@@ -0,0 +1,4 @@
+// Honestly, I have absolutely no idea why every dev seems to start everything off with "hello, world", but sure.
+// Open this file up in your developer console!
+
+console.log("Hello, World!");
