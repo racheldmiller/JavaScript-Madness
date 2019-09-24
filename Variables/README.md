547335f1334b980101b639aac751c0e94b281daa
racheldmiller <rachelmiller415@gmail.com>
2019-09-23 23:30:03 -0700

    created variable example

---
+++ Variables/README.md
@@ -0,0 +1,3 @@
+Variables are cool because rather than repeating yourself consistently, you can save a value into a variable and just reference the variable.
+
+Wait. What?
---
+++ Variables/script.js
@@ -0,0 +1,33 @@
+// --------------------------- QUESTION ---------------------------------
+// What are some types of values we could have when using variables? And what do they mean?
+
+// BOOLEAN: true or false
+// NULL: non-existent
+// NUMBERS: integers
+// STRING: a bunch of characters
+// UNDEFINED: no value yet
+
+// ------------------------------ EXAMPLE ----------------------------------
+// For demonstrative purposes, I'm going to give an example of a STRING, NUMBER, and BOOLEAN.
+
+// STRING
+// Note: Notice how if it's a string, it needs to be in quotes?
+var name = "Ryan";
+
+// NUMBER
+// Note: Notice how I can write out the integer without quotes?
+var age = 25;
+
+// BOOLEAN
+// Note: in this case, it's true. Alternatively, I could set this to false.
+var isDeveloper = true;
+
+// ---------------------------- TESTING -----------------------------------
+var name = "Ryan";
+console.log(name);
+
+var age = 25;
+console.log(age);
+
+var isDeveloper = true;
+console.log(isDeveloper);
