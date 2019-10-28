f2742db45c23804925fbfad2cb30826434b4968e
racheldmiller <rachelmiller415@gmail.com>
2019-10-27 19:25:33 -0700

    finished off example

--- Variables: Mutated/script.js
+++ Variables: Mutated/script.js
@@ -18,6 +18,39 @@ var job, isSingle;
 
 console.log(isSingle); // This will return as undefined because we never defined what isSingle is. What is isSingle? A boolean?
 
-// Ok, I should probably define both.
+// Ok, I should probably declare and define both.
 var job = "developer"; // I just love my developers.
 var isSingle = false; // Ah yes, this is a boolean!
+
+// Person 1: "Hi, what can you tell me about Ryan?"
+// Person 2: "Well, I can tell you that..."
+console.log(
+  "His name is " +
+    name +
+    " he's " +
+    age +
+    " he works as a " +
+    job +
+    " is he single? " +
+    isSingle
+);
+
+// At this point, pretend we're tired of dealing with Ryan. This is where mutation comes in handy.
+// Since we've using the same variables, we don't need to declare using "var" again.
+name = "Will";
+age = 22;
+isSingle = true;
+
+// Now that we've done some tweaking, let's using the same console.log structure to see what happens.
+console.log(
+  "His name is " +
+    name +
+    " he's " +
+    age +
+    " he works as a " +
+    job +
+    " is he single? " +
+    isSingle
+);
+
+// How cool is that?!
